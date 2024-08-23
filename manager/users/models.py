45d511af492309715913b627ced78b2b4c7744a9
CodeConnoisseur74 <sherry.andrews.com>
2024-08-23 17:13:45 +0200

    update users/models and views with username validator

--- manager/users/models.py
+++ manager/users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser, UserManager
+from django.core.validators import RegexValidator
 from django.db import models
 from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserManager(UserManager):
@@ -12,5 +14,16 @@ class CustomUserManager(UserManager):
 
 
 class CustomUser(AbstractUser):
+    username_validator = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Only alphanumeric characters are allowed.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _('A user with that username already exists.'),
+        },
+    )
     email = models.EmailField(blank=False, unique=True)
     objects = CustomUserManager()
