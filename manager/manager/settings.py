4c75e67e9ebe4ded970834d268fcbb8cc8b1dfd2
CodeConnoisseur74 <sherry.andrews.com>
2024-08-23 15:34:30 +0200

    Fix pyproject.toml and add permissions to wins app

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -68,6 +68,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication',),
+}
+
 ROOT_URLCONF = 'manager.urls'
 
 TEMPLATES = [
--- manager/users/models.py
+++ manager/users/models.py
@@ -1,8 +1,16 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+from django.db.models import Q
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+    def get_by_natural_key(self, username):
+        return self.get(
+            Q(**{self.model.USERNAME_FIELD: username}),
+            Q(**{self.model.EMAIL_FIELD: username}),
+        )
 
 
 class CustomUser(AbstractUser):
     email = models.EmailField(blank=False, unique=True)
+    objects = CustomUserManager()
