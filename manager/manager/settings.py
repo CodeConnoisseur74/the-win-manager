58591aa735a46a9485c0e186fc5ecaef8ef5cc1b
CodeConnoisseur74 <sherry.andrews.com>
2024-09-06 18:46:44 +0200

    re run pip requirements and add Procfile and whitenoise

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'djoser',
     'users',
     'wins',
-    'frontend'
+    'frontend',
 ]
 
 GOOGLE_RECAPTCHA_SECRET = env('GOOGLE_RECAPTCHA_SECRET', default='')
@@ -69,6 +69,7 @@ DJOSER = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -150,7 +151,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
